ea5c2b35fc578613a5541846f637ba3cd9006bd8
ProgrammingMuse <riverperera@gmail.com>
2025-02-19 15:13:34 -0500

    this is river's file

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RiverPereraHW2.java
@@ -0,0 +1,20 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+public class RiverPereraHW2 {
+
+    public int camelCase = 100;
+    //int camelCase = 200;
+    private double notAnInteger = 1.01;
+    double happensToBeAnInteger = 1;
+    String someFunWords = "Hello World";
+
+    //"left_front_motor"
+    final int CONSTANT_VARIABLE = 100;
+    public DcMotorSimple leftFrontMotor;
+    // this is a comment
+    /*
+    group comment
+     */
+}
