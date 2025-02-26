dbff6a3559cf960c0de757df142869b9b4edc5d8
jayebirk <jayebirk@gmail.com>
2025-02-26 14:12:37 -0500

    Create JayeBirkinHW2.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/JayeBirkinHW2.java
@@ -0,0 +1,25 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+public class JayeBirkinHW2 {
+
+    public int camelCase = 45843;
+    int numberEqualToTwoHundred = 200;
+    private double Tomatoes = 32944.5432;
+    String name = "Jaye marcus Brikin";
+    final int constant = 7;
+
+    //left front motor
+    public DcMotorSimple leftFrontMotor;
+
+    /*
+    Public vs Private Objects:
+       - Public objects can be accessed from outside the class
+       - Private objects can only be accessed within the class
+
+    Static vs Mutable Objects:
+       - Static objects belong to the class itself and are shared among all instances
+       - Mutable objects can be changed after creation
+    */
+}
