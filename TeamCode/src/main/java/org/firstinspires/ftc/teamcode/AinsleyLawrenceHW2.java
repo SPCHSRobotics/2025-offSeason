6461084277511ec95af99717a3a4d65619924b10
AinsleyLaw <ainsleylawr07@gmail.com>
2025-02-26 07:52:46 -0500

    AinsleyLawrenceHW2
    I do not know what I am doing, I think I did this right, maybe.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AinsleyLawrenceHW2.java
@@ -0,0 +1,18 @@
+package org.firstinspires.ftc.teamcode;
+
+public class AinsleyLawrenceHW2 {
+    String name = "Taiwan";
+//This is a string, it allows you to put words into the program
+    int thisIsNumbers = 1770;
+//This allows you to put a number into the program
+    double computerBattery = 0.95;
+//Doubles allow you to
+    DcMotor frontJohnnyMotor = 200;
+//This controls the motor
+    Enum name = 20;
+//this defines a fixed set of constants
+    Servo pleaseHelp = 90;
+//Servo is a motor I think, the number controls the degree it spins, maybe
+    Constant moreNumbers = 2;
+//This is a number that remains the same throughout the process
+}
