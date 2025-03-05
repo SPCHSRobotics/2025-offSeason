de70d12e21b48c8800d247afa49f0731eb4a21ea
AinsleyLaw <ainsleylawr07@gmail.com>
2025-03-05 11:46:21 -0500

    AinsleyLawrenceHW2.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AinsleyLawrenceHW2.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AinsleyLawrenceHW2.java
@@ -2,15 +2,18 @@ package org.firstinspires.ftc.teamcode;
 
 public class AinsleyLawrenceHW2 {
     String name = "Taiwan";
+    System.out.println(name)
 //This is a string, it allows you to put words into the program
     int thisIsNumbers = 1770;
+    System.out.println(thisIsNumbers)
 //This allows you to put a number into the program
     double computerBattery = 0.95;
-//Doubles allow you to
+    System.out.println(computerBattery)
+//Doubles allow you to hold precise decimals
     DcMotor frontJohnnyMotor = 200;
-//This controls the motor
-    Enum name = 20;
-//this defines a fixed set of constants
+//This controls the motor and how much power is put into it
+    Enum Level = Low;
+//this represents a group of constants 
     Servo pleaseHelp = 90;
 //Servo is a motor I think, the number controls the degree it spins, maybe
     Constant moreNumbers = 2;
