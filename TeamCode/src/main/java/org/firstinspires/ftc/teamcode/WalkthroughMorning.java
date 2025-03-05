1256c4708908cc01db4e5ba3147abe311b6d9ee8
ProgrammingMuse <riverperera@gmail.com>
2025-03-05 09:31:22 -0500

    This is River's morning tutorial walkthrough

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/WalkthroughMorning.java
@@ -0,0 +1,39 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+
+public class WalkthroughMorning {
+    // This is a comment
+
+    /*
+    This is a group comment
+
+    Welcome to robotics
+
+
+     */
+    int ironVulcan = 27095; // addendum
+    final int TECH_TITANS = 24257;
+    double fieldCoordinate = 72.87;
+    String userMessage = "Welcome to Robotics";
+    DcMotor leftFrontMotor;
+    Servo claw;
+
+    public void runOpMode() {
+        //ironVulcan = 24527;
+        ironVulcan = 101;
+
+        if (fieldCoordinate == 27095) {
+            int ironVulcan = 191;
+        }
+
+    }
+
+    public int addition(int firstNumber, int secondNumber) {
+        int sum = firstNumber + secondNumber;
+
+        return sum;
+    }
+
+}
