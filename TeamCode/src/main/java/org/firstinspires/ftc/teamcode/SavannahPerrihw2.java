dbb76c91131c4fec11e0f281c81481f13360d735
savnic0le <serenanicole415@gmail.com>
2025-03-04 22:54:11 -0500

    sav hw 2

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SavannahPerrihw2.java
@@ -0,0 +1,65 @@
+package org.firstinspires.ftc.teamcode;
+
+public class SavannahPerrihw2 {
+}
+// enum is short for enumerations, and represents a group of constants
+enum Color {
+    BLUE,
+    RED,
+    GREEN
+}
+
+
+public class Main {
+    public static void main(String[] args) {
+        Color myVar = Color.GREEN;
+        System.out.println(myVar);
+    }
+}
+
+
+// Int (short for integer) represents whole numbers like 20 or 50 and has no decimals or fractions
+public class Main {
+    public static void main(String[] args) {
+        int myAge;
+        myAge = 18;
+        System.out.println(myAge);
+    }
+}
+
+
+// Double represents fraction numbers or numbers with decimals
+public class Main {
+    public static void main(String[] args) {
+        double myNum = 1.2345d;
+        System.out.println(myNum);
+    }
+}
+
+
+// String is a sequence of characters and are in double quotes
+public class Main {
+    public static void main(String[] args) {
+        string greeting = "Hello World";
+        System.out.println(greeting);
+    }
+}
+// Constants have fixed values that cannot be changed
+package main
+import ("fmt")
+
+
+        const A int = 2
+
+
+func main() {
+    fmt.Println(A)
+}
+
+
+// Direct current (dc) motor converts current electrical energy into mechanical energy
+
+
+// Servo motors control motions
+
+
