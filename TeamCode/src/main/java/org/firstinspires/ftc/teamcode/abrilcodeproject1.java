aeeb25e2162d2cf51d819f7446f8ca8818d2945f
ProgrammingMuse <riverperera@gmail.com>
2025-03-26 14:20:53 -0400

    Color sensor code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/abrilcodeproject1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/abrilcodeproject1.java
@@ -1,16 +1,19 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+
 public class abrilcodeproject1 {
 
     // int is a number
-    int ironVulcan = 27095
+    int ironVulcan = 27095;
             // double is a coordinate
     double fieldCoordinate= 72.87;
     // string is a word
-    String userMessage = "welcome"
+    String userMessage = "welcome";
             //DcMotor is  a specific mote
             DcMotor frontLeftMotor;
     //servomotor is a specific motor
-    servomotor frontRightMotor;
+    Servo frontRightMotor;
 
 }
